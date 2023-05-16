1e64f550352ef1adf14b2783f44fd8b23ced031e
SystematicVisualImagination <systematic-visual-imagination@proton.me>
2023-05-16 07:57:10 -0400

    data samples

--- samples/dSprites/deleter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -type d | while read -r dir; do
-    base_dir=$(basename "$dir")
-    if [[ $base_dir =~ ^0+ && 10#${base_dir} -ge 100 ]]; then
-        rm -rf "${dir}"
-    fi
-done
-
